9d2db74464adc29e263e619862a6bf7e16be6252
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-06-01 00:17:14 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import tweepy
+import geopy
+from geopy.geocoders import Nominatim
+import pandas as pd
+
+consumer_key = 'Uy4VXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+consumer_secret = 'Ewt9jXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token = '1040877050XXXXXXXXXXXXXXXXXXXXXX'
+access_token_secret = 'gM9TM4XXXXXXXXXXXXXXXXXXX'
+
